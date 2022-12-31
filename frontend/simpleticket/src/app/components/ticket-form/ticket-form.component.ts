7e1ab26830784773d61aca44aee2c4ad3f7cefba
Raffaele Spataro <raffaele2692@gmail.com>
2023-01-01 00:07:16 +0100

    improved create ticket with bootstrap typehead

--- frontend/simpleticket/src/app/components/ticket-form/ticket-form.component.ts
+++ frontend/simpleticket/src/app/components/ticket-form/ticket-form.component.ts
@@ -5,6 +5,14 @@ import { Ticket } from 'src/app/models/ticket';
 import { User } from 'src/app/models/user';
 import { NgForm } from '@angular/forms';
 
+import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+import { Observable, OperatorFunction } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+
+// TODO remove
+import { FormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+
 @Component({
   selector: 'app-ticket-create',
   templateUrl: './ticket-form.component.html',
@@ -13,7 +21,7 @@ import { NgForm } from '@angular/forms';
 
 export class TicketFormComponent implements OnInit {
 
-  users: User[] | undefined;
+  users!: User[];
   ticket!: Ticket;
 
   constructor(private userApi: UserService, private ticketApi: TicketService) { }
@@ -28,7 +36,12 @@ export class TicketFormComponent implements OnInit {
   }
 
   addTicket(form: NgForm) {
-    console.log(form.value)
+    console.log(form.value); // data collected from form
+    // TODO better handle this in form...
+    form.value["createdBy"] = form.value["createdBy"]["url"];
+    form.value["requestedBy"] = form.value["requestedBy"]["url"];
+    form.value["requestedFor"] = form.value["requestedFor"]["url"];
+    // ...
     this.ticketApi.addTicket(form.value).subscribe(
       data => {
         console.log(data);
@@ -39,4 +52,19 @@ export class TicketFormComponent implements OnInit {
     )
     form.reset()
   }
+
+  // typehead stuff
+  // TODO this would not be good if related users are a lot, should implement sothing else ...?
+  searchUser: OperatorFunction<string, readonly User[]> = (text$: Observable<string>) =>
+  
+  text$.pipe(
+    debounceTime(200),
+    distinctUntilChanged(),
+
+    map((term) =>
+      term.length < 1 ? [] : this.users.filter((user) => user.username.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10),
+    ),
+  );
+  userResultFormatter = (user: { email: string }) => user.email;
+  userInputFormatter = (user: { email: string }) => user.email;
 }
