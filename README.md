edb5c11f7a43f1f9d212d228a9825800b138d55b
Gabriele Bonaldo <gabrielebonaldo1@gmail.com>
2023-01-24 12:47:42 +0100

    bonaldo last version

--- README.md
+++ README.md
@@ -7,21 +7,17 @@ npm install
 ```
 
 ## todo
-- edit create form to handle 'edit mode'
-- create edit route
-- create delete route
-- add form validation
-- add edit/delete buttons to tiket-form
+- edit create form to handle 'edit mode' V
+- create edit route V
+- create delete route V
+- add form validation V
+- add edit/delete buttons to tiket-form V
 - add jwt token auth in django
 - add authentication service angular
-- add login pages angualr
+- add login pages angualr V
 - add pagination for tickets (django + angular)
-- add pagination/alternative method for user reference in ticket (angular)  <--- partially achieved with angular bootstrap
-- restyle
+- add pagination/alternative method for user reference in ticket (angular)  <--- partially achieved with angular bootstrap V
+- restyle V
 
 ## bugs
 - time zone is different between angular and django
-
-## do
-- button delete (10/01/2022)
-- restyle (11/01/2022)
--- frontend/simpleticket/src/app/app-routing.module.ts
+++ frontend/simpleticket/src/app/app-routing.module.ts
@@ -4,17 +4,20 @@ import { HomeComponent } from './components/home/home.component';
 import { TicketAllComponent } from './components/ticket-all/ticket-all.component';
 import { TicketFormComponent } from './components/ticket-form/ticket-form.component';
 import { TicketComponent } from './components/ticket/ticket.component';
+import { LoginComponent } from './auth/login/login.component';
+
 const routes: Routes = [
-  { path :  '', component : HomeComponent}, 
+  { path :  '', component : LoginComponent}, 
   { path : 'ticketall', component : TicketAllComponent},
-  { path: 'ticketcreate', component: TicketFormComponent},
-  { path: 'ticketcreate/:ticket:edit', component: TicketFormComponent},
-  { path: 'ticketdetail', component: TicketComponent},
+  { path : 'ticketcreate', component: TicketFormComponent},
+  { path : 'ticketcreate/:ticket:edit', component: TicketFormComponent},
+  { path : 'ticketdetail', component: TicketComponent},
   // { path: 'tickets/ticket/edit', component : TicketFormComponent},
 ];  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
