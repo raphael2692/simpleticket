2e0b3745f863c9644678b3221b6dd52cf0943148
Raffaele Spataro <raffaele2692@gmail.com>
2023-02-03 11:53:38 +0100

    enabled hot reload django

--- backend/start.sh
+++ backend/start.sh
@@ -9,4 +9,4 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser --noinput --username admin --email admin@email.com  --skip-checks
 
-gunicorn simpleticket.wsgi:application --bind 0.0.0.0:8000
+gunicorn simpleticket.wsgi:application --bind 0.0.0.0:8000 --reload
